74b10aa611e42da78a8b33ce35ef5a1524d98269
Yusra Atta <yvvsra1@gmail.com>
2020-02-17 21:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+class Polygon {
+    constructor(countSides)
+    {
+        this.countSides=countSides;
+    }
+    
+    get countSides()
+    {
+    return this.countSides.length;
+    }
+    
+    get perimeter()
+    {
+        let sum=0
+        for(let side of this.countSides)
+        {
+            sum=sum+side;
+        }
+    
+        return sum;
+    }
+
+}
